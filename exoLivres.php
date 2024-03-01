e28988c55a409d3763ed9526945c6f365f3d2d2a
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-03-01 09:07:25 +0100

    Mise en place exo livres POO

---
+++ exoLivres.php
@@ -0,0 +1,16 @@
+<h1>Exercice livres</h1>
+
+<p>
+Vous êtes chargé(e) de créer un projet orienté objet permettant de gérer des livres et 
+leurs auteurs respectifs.
+Les livres sont caractérisés par un titre, un nombre de pages, une année de parution, un prix et un 
+auteur. Un auteur comporte un nom et un prénom.
+Une méthode toString() sera appréciée dans chacune de vos classes.
+Vous implémenterez une méthode afficherBibliographie() qui permettra d'afficher la bibliographie 
+complète d'un auteur :
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+
